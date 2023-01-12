5b3b412b5566291749d426f99164404f6635d911
Shaikh-wasima <88181032+Shaikh-wasima@users.noreply.github.com>
2023-01-12 20:56:12 +0530

    Update script.js

--- Portfolio/assests/js/script.js
+++ Portfolio/assests/js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Portfolio | Saurav Mukherjee";
+        document.title = "Portfolio | Wasima Shaikh";
         $("#favicon").attr("href","assests/images/favicon.png");
     }
     else {
