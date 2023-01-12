0b6ba6273657b12a106b46af547a488309083a87
Shaikh-wasima <88181032+Shaikh-wasima@users.noreply.github.com>
2023-01-13 01:54:38 +0530

    Update script.js

--- Portfolio/assests/js/script.js
+++ Portfolio/assests/js/script.js
@@ -53,7 +53,7 @@ function(){
 
 // <!-- typed js effect starts -->
     var typed = new Typed(".typing-text", {
-        strings: ["Full-Stack development", "Machine Learning", "Cloud Computing", "Tech Communities"],
+        strings: ["Web development", "Machine Learning", "Cloud Computing", "Tech Communities"],
         loop: true,
         typeSpeed: 50,
 		backSpeed: 25,
